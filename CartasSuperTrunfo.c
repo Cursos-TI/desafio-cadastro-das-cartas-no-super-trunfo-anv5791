be2f83c363e3865892eaa1f020ffbeb500133b87
anv5791 <32498826+anv5791@users.noreply.github.com>
2025-02-20 00:19:29 +0000

    adicionado código das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -461,179 +461,182 @@ int main() {
 
     //Mostra as informações digitadas
 
+    printf("Códigos das Cartas (%s)\n", mostracodigos);
+    printf("\n");
+
     printf("Carta %s\n", codigocidade[0]);
     printf("Cidade: %s\n", nomecidade[0]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[0]);
     printf("Área Total: %.2f km² \n", area[0]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[0], pib[0]);
-    printf("Número de pontos turísticos: %d\n", numeropontoturistico[0]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[0], pib[0]);
+    printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[0]);
         
     printf("Carta %s\n", codigocidade[1]);
     printf("Cidade: %s\n", nomecidade[1]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[1]);
     printf("Área Total: %.2f Km\n", area[1]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[1], pib[1]);
-    printf("Número de pontos turísticos: %d\n", numeropontoturistico[1]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[1], pib[1]);
+    printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[1]);
     
     printf("Carta %s\n", codigocidade[2]);
     printf("Cidade: %s\n", nomecidade[2]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[2]);
     printf("Área Total: %.2f Km\n", area[2]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[2], pib[2]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[2], pib[2]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[2]);
    
     printf("Carta %s\n", codigocidade[3]);
     printf("Cidade: %s\n", nomecidade[3]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[3]);
     printf("Área Total: %.2f Km\n", area[3]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[3], pib[3]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[3], pib[3]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[3]);
     
     printf("Carta %s\n", codigocidade[4]);
     printf("Cidade: %s\n", nomecidade[4]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[4]);
     printf("Área Total: %.2f Km\n", area[4]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[4], pib[4]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[4], pib[4]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[4]);
     
     printf("Carta %s\n", codigocidade[5]);
     printf("Cidade: %s\n", nomecidade[5]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[5]);
     printf("Área Total: %.2f Km\n", area[5]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[5], pib[5]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[5], pib[5]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[5]);
     
     printf("Carta %s\n", codigocidade[6]);
     printf("Cidade: %s\n", nomecidade[6]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[6]);
     printf("Área Total: %.2f Km\n", area[6]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[6], pib[6]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[6], pib[6]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[6]);
     
     printf("Carta %s\n", codigocidade[7]);
     printf("Cidade: %s\n", nomecidade[7]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[7]);
     printf("Área Total: %.2f Km\n", area[7]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[7], pib[7]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[7], pib[7]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[7]);
    
     printf("Carta %s\n", codigocidade[8]);
     printf("Cidade: %s\n", nomecidade[8]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[8]);
     printf("Área Total: %.2f Km\n", area[8]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[8], pib[8]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[8], pib[8]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[8]);
     
     printf("Carta %s\n", codigocidade[9]);
     printf("Cidade: %s\n", nomecidade[9]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[9]);
     printf("Área Total: %.2f Km\n", area[9]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[9], pib[9]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[9], pib[9]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[9]);
     
     printf("Carta %s\n", codigocidade[10]);
     printf("Cidade: %s\n", nomecidade[10]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[10]);
     printf("Área Total: %.2f Km\n", area[10]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[10], pib[10]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[10], pib[10]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[10]);
    
     printf("Carta %s\n", codigocidade[11]);
     printf("Cidade: %s\n", nomecidade[11]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[11]);
     printf("Área Total: %.2f Km\n", area[11]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[11], pib[11]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[11], pib[11]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[11]);
    
     printf("Carta %s\n", codigocidade[12]);
     printf("Cidade: %s\n", nomecidade[12]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[12]);
     printf("Área Total: %.2f Km\n", area[12]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[12], pib[12]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[12], pib[12]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[12]);
     
     printf("Carta %s\n", codigocidade[13]);
     printf("Cidade: %s\n", nomecidade[13]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[13]);
     printf("Área Total: %.2f Km\n", area[13]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[13], pib[13]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[13], pib[13]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[13]);
    
     printf("Carta %s\n", codigocidade[14]);
     printf("Cidade: %s\n", nomecidade[14]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[14]);
     printf("Área Total: %.2f Km\n", area[14]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[14], pib[14]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[14], pib[14]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[14]);
     
     printf("Carta %s\n", codigocidade[15]);
     printf("Cidade: %s\n", nomecidade[15]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[15]);
     printf("Área Total: %.2f Km\n", area[15]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[15], pib[15]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[15], pib[15]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[15]);
     
     printf("Carta %s\n", codigocidade[16]);
     printf("Cidade: %s\n", nomecidade[16]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[16]);
     printf("Área Total: %.2f Km\n", area[16]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[16], pib[16]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[16], pib[16]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[16]);
     
     printf("Carta %s\n", codigocidade[17]);
     printf("Cidade: %s\n", nomecidade[17]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[17]);
     printf("Área Total: %.2f Km\n", area[17]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[17], pib[17]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[17], pib[17]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[17]);
     
     printf("Carta %s\n", codigocidade[18]);
     printf("Cidade: %s\n", nomecidade[18]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[18]);
     printf("Área Total: %.2f Km\n", area[18]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[18], pib[18]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[18], pib[18]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[18]);
     
     printf("Carta %s\n", codigocidade[19]);
     printf("Cidade: %s\n", nomecidade[19]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[19]);
     printf("Área Total: %.2f Km\n", area[19]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[19], pib[19]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[19], pib[19]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[19]);
     
     printf("Carta %s\n", codigocidade[20]);
     printf("Cidade: %s\n", nomecidade[20]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[20]);
     printf("Área Total: %.2f Km\n", area[20]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[20], pib[20]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[20], pib[20]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[20]);
     
     printf("Carta %s\n", codigocidade[21]);
     printf("Cidade: %s\n", nomecidade[21]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[21]);
     printf("Área Total: %.2f Km\n", area[21]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[21], pib[21]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[21], pib[21]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[21]);
     
     printf("Carta %s\n", codigocidade[22]);
     printf("Cidade: %s\n", nomecidade[22]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[22]);
     printf("Área Total: %.2f Km\n", area[22]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[22], pib[22]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[22], pib[22]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[22]);
     
     printf("Carta %s\n", codigocidade[23]);
     printf("Cidade: %s\n", nomecidade[23]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[23]);
     printf("Área Total: %.2f Km\n", area[23]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[23], pib[23]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[23], pib[23]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[23]);
     
     printf("Carta %s\n", codigocidade[24]);
     printf("Cidade: %s\n", nomecidade[24]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[24]);
     printf("Área Total: %.2f Km\n", area[24]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[24], pib[24]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[24], pib[24]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[24]);
     
     printf("Carta %s\n", codigocidade[25]);
@@ -682,7 +685,7 @@ int main() {
     printf("Cidade: %s\n", nomecidade[31]);
     printf("População total da cidade: %d milhões de pessoas\n", populacao[31]);
     printf("Área Total: %.2f Km\n", area[31]);
-    printf("PIB total da cidade de %s é: %.2lf milhões de dolares\n", nomecidade[31], pib[31]);
+    printf("PIB total da cidade de %s é: %.2lf trilhões USD\n", nomecidade[31], pib[31]);
     printf("Número de pontos turísticos: %d\n\n", numeropontoturistico[31]);
     printf("\n");
     
